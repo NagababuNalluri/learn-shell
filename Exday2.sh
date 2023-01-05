1a541230539d044e42cd59369d1a15063bb41a31
nagababu <nag4c1@gmail.com>
2023-01-05 09:45:32 +0530

    day 2 writing two commands

--- Exday2.sh
+++ Exday2.sh
@@ -1,4 +1,8 @@
 echo classes are very fast and furious not able to catch the points.
 
-echo -e "\e[31m what are you doing\e[0m
-"
+echo -e "\e[31m what are you doing\e[0m"
+
+echo -e "\e[32m i am basically learner\e[0m"
+
+echo -e "\e[33m i am basically learner and strongly believe never stop learning\e[0m"
+
