cbdd53ceb2dcdabc4f51837530690cb8d2c34ade
nagababu <nag4c1@gmail.com>
2023-01-07 17:21:23 +0530

    reverseproxy vim file

--- 1-printing.sh
+++ 1-printing.sh
@@ -1,26 +1,5 @@
-echo "$(date +%F)"
-
-pwd
-
-a=Nagababu
-b=Lalithakumari
-c=Hemasri
-
-
-echo My name is $a
-
-echo My name is $b
-
-echo My name is $c
-
-
-
-echo My wife name lalitha kumari
-
-
-for i in {1..35..2}; do
-  echo print "$i"
+for i in {1..100..4} ; do
+  echo $i
   done
-
 
 
