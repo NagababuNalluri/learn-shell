bb49c84e35f137f9a1cd55790d59a662ed50a0a8
nagababu <nag4c1@gmail.com>
2023-01-07 17:35:02 +0530

    MODIFIED

--- commonone.sh
+++ commonone.sh
@@ -2,9 +2,9 @@ echo -e  "\e[1;31mhello\e[0m"
 
 echo -e  "\e[1;32mhello\e[0m"
 
-echo -e  "\e[33mhello\e[0m"
+echo -e  "\e[2;33mhello\e[0m"
 
-echo -e  "\e[34mhello\e[0m"
+echo -e  "\e[2;34mhello\e[0m"
 
 echo -e  "\e[35mhello\e[0m"
 
