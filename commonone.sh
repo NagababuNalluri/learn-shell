559001076772ec5f061e6e0aa621da69a0dbb142
nagababu <nag4c1@gmail.com>
2023-01-07 21:19:35 +0530

    MODIFIED

--- commonone.sh
+++ commonone.sh
@@ -10,3 +10,18 @@ echo -e  "\e[35mhello\e[0m"
 
 echo -e  "\e[36mhello\e[0m"
 
+a=(1 2 3 4 5 6 7)
+
+echo ${a[0]}
+
+echo ${a[2]}
+
+echo ${a[3]}
+
+echo ${a[4]}
+
+echo ${a[5]}
+
+echo ${a[6]}
+
+echo ${a[7]}
